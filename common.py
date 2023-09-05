d8a74460383f895070ff6f6c5c9cb82b0a2350e5
Dustin Wei(韦伟) <dustin.wei@QUECTEL.COM>
2023-09-05 15:18:16 +0800

    common

--- common.py
+++ common.py
@@ -1,10 +1,22 @@
+import utime
+import usys
 import _thread
 import osTimer
+from queue import Queue
+
+
+Lock = _thread.allocate_lock
+
+
+class TimeoutError(Exception):
+    pass
+
+
+class FullError(Exception):
+    pass
 
 
 class Singleton(object):
-    """单例装饰"""
-
     def __init__(self, cls):
         self.cls = cls
         self.instance = None
@@ -22,50 +34,59 @@ class Waiter(object):
 
     def __init__(self):
         self.__lock = _thread.allocate_lock()
-        self.__lock.acquire()  # acquire immediately for holding the lock.
+        self.__lock.acquire()
         self.__unlock_timer = osTimer()
+        self.__is_timeout = False
 
     def __auto_unlock(self, _):
-        self.notify()
+        self.__is_timeout = True
+        self.release()
 
-    def wait(self, timeout=-1):
+    def acquire(self, timeout=-1):
         if timeout > 0:
             self.__unlock_timer.start(timeout * 1000, 0, self.__auto_unlock)
-        self.__lock.acquire()
-        self.__lock.release()
+        self.__lock.acquire()  # block until timeout or release
         self.__unlock_timer.stop()
+        if self.__lock.locked():
+            self.__lock.release()
+        if self.__is_timeout:
+            raise TimeoutError
 
-    def notify(self):
+    def release(self):
         if self.__lock.locked():
             self.__lock.release()
 
 
 class Condition(object):
-    """条件变量(使用互斥锁实现)"""
 
     def __init__(self):
         self.__lock = _thread.allocate_lock()
         self.__waiters = []
 
-    def __create_waiter(self):
+    def wait(self, timeout=-1):
         waiter = Waiter()
         with self.__lock:
             self.__waiters.append(waiter)
-        return waiter
 
-    def wait(self, timeout=-1):
-        waiter = self.__create_waiter()
-        waiter.wait(timeout)
-        with self.__lock:
-            self.__waiters.remove(waiter)
+        try:
+            waiter.acquire(timeout)  # block until timeout or notify
+        except TimeoutError as exc:
+            raise exc
+        finally:
+            with self.__lock:
+                self.__waiters.remove(waiter)
 
     def notify(self, n=1):
+        if n <= 0:
+            raise ValueError('invalid param, n should be > 0.')
         with self.__lock:
             for waiter in self.__waiters[:n]:
-                waiter.notify()
+                waiter.release()
 
     def notify_all(self):
-        self.notify(n=len(self.__waiters))
+        with self.__lock:
+            for waiter in self.__waiters:
+                waiter.release()
 
 
 class Event(object):
@@ -75,9 +96,10 @@ class Event(object):
         self.flag = False
         self.cond = Condition()
 
-    def wait(self):
-        if self.flag is not True:
-            self.cond.wait()
+    def wait(self, timeout=-1):
+        """wait until internal flag is True"""
+        if not self.is_set():
+            self.cond.wait(timeout)
         return self.flag
 
     def set(self):
@@ -90,62 +112,135 @@ class Event(object):
             self.flag = False
 
     def is_set(self):
-        return self.flag
+        with self.__lock:
+            return self.flag
 
-            
 
-# 单次定时器（自动关闭）
-class OneShotTimer(object):
-
-    def __init__(self, callback=None):
-        self.timer = osTimer()
-
-        def do_nothing(args):
-            pass
-
-        self.user_callback = callback or do_nothing
-
-    def start(self, period):
-        self.timer.start(period, 0, self.cb)
-
-    def interrupt(self, do=True):
-        self.timer.stop()
-        if do:
-            self.user_callback(None)
-
-    def cb(self, args):
-        self.user_callback(args)
-        self.timer.stop()  # 必须关闭，否则下一次定时器无法触发回调
-        
-
-# 实例方法锁
-class MutexMethod(object):
-    default_lock = _thread.allocate_lock()
-
-    def __init__(self, lock=None):
-        self.lock = lock or self.default_lock
-
-    def __call__(self, method):
-        def wrapper(*args, **kwargs):
-            with self.lock:
-                return method(*args, **kwargs)
-        return wrapper
-        
-
-class Mutex(object):
+class _Result(object):
 
     def __init__(self):
-        self.__lock = _thread.allocate_lock()
-        self.__unlock_timer = osTimer()
+        self.__rv = None
+        self.__exc = None
+        self.__finished = Event()
 
-    def __auto_unlock(self, args):
-        self.release()
+    def set(self, exc, rv):
+        self.__exc = exc
+        self.__rv = rv
+        self.__finished.set()
 
-    def acquire(self, timeout=-1):
-        if timeout > 0:
-            self.__unlock_timer.start(timeout*1000, 0, self.__auto_unlock)
-        self.__lock.acquire()
+    def get(self, timeout=-1):
+        self.__finished.wait(timeout=timeout)
+        if self.__exc:
+            raise self.__exc
+        return self.__rv
 
-    def release(self):
-        if self.__lock.locked():
-            self.__lock.release()
+
+class Thread(object):
+
+    def __init__(self, target=None, args=(), kwargs=None):
+        self.__target = target
+        self.__args = args
+        self.__kwargs = kwargs or {}
+        self.__worker_thread_id = None
+        self.__lock = Lock()
+
+    def __repr__(self):
+        return '<Thread {}>'.format(self.__worker_thread_id)
+
+    def is_running(self):
+        return self.__worker_thread_id and _thread.threadIsRunning(self.__worker_thread_id)
+
+    def start(self, delay=-1):
+        if not self.is_running():
+            if delay > 0:
+                utime.sleep(delay)
+            result = _Result()
+            self.__worker_thread_id = _thread.start_new_thread(self.run, (result, ))
+            return result
+
+    def stop(self):
+        if self.is_running():
+            _thread.stop_thread(self.__worker_thread_id)
+            self.__worker_thread_id = None
+
+    def run(self, result):
+        try:
+            rv = self.__target(*self.__args, **self.__kwargs)
+        except Exception as e:
+            result.set(e, None)
+        else:
+            result.set(None, rv)
+
+
+class _WorkItem(object):
+
+    def __init__(self, fn, args, kwargs):
+        self.__fn = fn
+        self.__args = args
+        self.__kwargs = kwargs
+        self.result = _Result()
+
+    def run(self):
+        try:
+            rv = self.__fn(*self.__args, **self.__kwargs)
+        except Exception as e:
+            self.result.set(e, None)
+        else:
+            self.result.set(rv, None)
+
+
+class ThreadPoolExecutor(object):
+
+    def __init__(self, max_workers=10):
+        if max_workers <= 0:
+            raise ValueError("max_workers must be greater than 0")
+        self.__max_workers = max_workers
+        self.__worker_queue = Queue()
+        self.__threads = set()
+        self.__lock = Lock()
+
+    def submit(self, fn, *args, **kwargs):
+        item = _WorkItem(fn, args, kwargs)
+        self.__worker_queue.put(item)
+        self.__adjust_thread_count()
+        return item.result
+
+    def __adjust_thread_count(self):
+        with self.__lock:
+            if len(self.__threads) < self.__max_workers:
+                t = Thread(target=self.__worker, args=(self.__worker_queue, ))
+                t.start()
+                self.__threads.add(t)
+
+    @staticmethod
+    def __worker(worker_queue):
+        while True:
+            try:
+                item = worker_queue.get()
+                item.run()
+            except Exception as e:
+                usys.print_exception(e)
+
+    def shutdown(self):
+        with self.__lock:
+            for t in self.__threads:
+                t.stop()
+
+
+class PubSub(object):
+    TOPIC_MAP = {}
+    PUBSUB_LOCK = Lock()
+
+    @classmethod
+    def subscribe(cls, topic, callback):
+        with cls.PUBSUB_LOCK:
+            if topic not in cls.TOPIC_MAP:
+                cls.TOPIC_MAP[topic] = [callback]
+            else:
+                cls.TOPIC_MAP[topic].append(callback)
+
+    @classmethod
+    def publish(cls, topic, *args, **kwargs):
+        with cls.PUBSUB_LOCK:
+            for cb in cls.TOPIC_MAP.get(topic, []):
+                Thread(target=cb, args=args, kwargs=kwargs).start()
--- thread.py
+++ thread.py
@@ -1,4 +1,81 @@
 import _thread
+import osTimer
+
+
+class Waiter(object):
+
+    def __init__(self):
+        self.__lock = _thread.allocate_lock()
+        self.__lock.acquire()
+        self.__unlock_timer = osTimer()
+
+    def __auto_unlock(self, _):
+        self.notify()
+
+    def wait(self, timeout=-1):
+        if timeout > 0:
+            self.__unlock_timer.start(timeout * 1000, 0, self.__auto_unlock)
+        self.__lock.acquire()  # block until timeout or notify
+        self.__lock.release()
+        self.__unlock_timer.stop()
+
+    def notify(self):
+        if self.__lock.locked():
+            self.__lock.release()
+
+
+class Condition(object):
+
+    def __init__(self):
+        self.__lock = _thread.allocate_lock()
+        self.__waiters = []
+
+    def wait(self, timeout=-1):
+        waiter = Waiter()
+        with self.__lock:
+            self.__waiters.append(waiter)
+        waiter.wait(timeout)  # block until timeout or notify
+        with self.__lock:
+            self.__waiters.remove(waiter)
+
+    def notify(self, n=1):
+        if n <= 0:
+            raise ValueError('invalid param, n should be > 0.')
+        with self.__lock:
+            for waiter in self.__waiters[:n]:
+                waiter.notify()
+
+    def notify_all(self):
+        with self.__lock:
+            for waiter in self.__waiters:
+                waiter.notify()
+
+
+class Event(object):
+
+    def __init__(self):
+        self.__lock = _thread.allocate_lock()
+        self.flag = False
+        self.cond = Condition()
+
+    def wait(self):
+        """wait until internal flag is True"""
+        if not self.flag:
+            self.cond.wait()
+        return self.flag
+
+    def set(self):
+        with self.__lock:
+            self.flag = True
+            self.cond.notify_all()
+
+    def clear(self):
+        with self.__lock:
+            self.flag = False
+
+    def is_set(self):
+        return self.flag
+
 
 class Thread(object):
 
@@ -22,3 +99,60 @@ class Thread(object):
 
     def run(self):
         self.__target(*self.__args, **self.__kwargs)
+
+
+class TimeoutError(Exception):
+    pass
+
+
+class Future(object):
+
+    def __init__(self):
+        self.__rv = None
+        self.__exc = None
+        self.__finished = False
+        self.__cond = Condition()
+
+    def set(self, exc, rv):
+        self.__exc = exc
+        self.__rv = rv
+        self.__finished = True
+        self.__cond.notify_all()
+
+    def get_result(self, timeout=-1):
+        self.__cond.wait(timeout=timeout)
+        if not self.__finished:
+            raise TimeoutError
+        if self.__exc:
+            raise self.__exc
+        return self.__rv
+
+
+class _Async(object):
+
+    def __init__(self, target=None, args=(), kwargs=None):
+        self.__target = target
+        self.__args = args
+        self.__kwargs = kwargs or {}
+
+    def delay(self):
+        f = Future()
+        _thread.start_new_thread(self.run, (f, ))
+        return f
+
+    def run(self, future):
+        try:
+            rv = self.__target(*self.__args, **self.__kwargs)
+        except Exception as e:
+            future.set(e, None)
+        else:
+            future.set(None, rv)
+
+
+class Async(object):
+
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __call__(self, *args, **kwargs):
+        return _Async(target=self.fn, args=args, kwargs=kwargs).delay()
